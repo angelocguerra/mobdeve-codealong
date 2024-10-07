7a07420fbf03eab83574d654ba566e88cbbd8867
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2024-10-08 02:40:08 +0800

    Added files from CA02

---
+++ CA02Sep24Intents/app/src/main/java/com/example/ca02sep24intents/SecondActivity.java
@@ -0,0 +1,38 @@
+package com.example.ca02sep24intents;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView outputText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        outputText = findViewById(R.id.outputText);
+        Intent i = getIntent();
+        String txt = i.getStringExtra("val1");
+        outputText.setText("Obtained from previous activity: " + txt);
+    }
+
+    public void btnClicked(View v) {
+        finish();
+    }
+}
